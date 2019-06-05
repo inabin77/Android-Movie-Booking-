49797cfccb5cb4e842f57ca8d14d603c1c7bdc28
Nabin Bhandari <inabin77@gmail.com>
2019-06-05 20:57:09 +0545

    Added Navigation Bar

---
+++ app/src/main/java/com/example/onlinecinematicketbookingsystem/LoginActivity.java
@@ -0,0 +1,43 @@
+package com.example.onlinecinematicketbookingsystem;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button login;
+    TextView reg;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        register();
+        login();
+    }
+
+    public void register(){
+        reg = findViewById(R.id.txtRegisterBtn);
+        reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void login(){
+        login = findViewById(R.id.btnLog);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,DashboardActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
