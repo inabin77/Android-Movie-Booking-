c3f0e006e957f4523b5aaf90357b35d08ca396e0
Nabin Bhandari <inabin77@gmail.com>
2019-06-16 12:14:45 +0545

    edited the file

--- app/src/main/java/com/example/onlinecinematicketbookingsystem/ui/main/LoginActivity.java
+++ app/src/main/java/com/example/onlinecinematicketbookingsystem/ui/main/LoginActivity.java
@@ -1,10 +1,12 @@
-package com.example.onlinecinematicketbookingsystem;
+package com.example.onlinecinematicketbookingsystem.ui.main;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.example.onlinecinematicketbookingsystem.R;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -20,7 +22,7 @@ public class LoginActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(LoginActivity.this,RegisterActivity.class);
+                Intent intent=new Intent(LoginActivity.this, RegisterActivity.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/onlinecinematicketbookingsystem/ui/main/RegisterActivity.java
+++ app/src/main/java/com/example/onlinecinematicketbookingsystem/ui/main/RegisterActivity.java
@@ -1,21 +1,13 @@
-package com.example.onlinecinematicketbookingsystem;
+package com.example.onlinecinematicketbookingsystem.ui.main;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.util.Log;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
+
+import com.example.onlinecinematicketbookingsystem.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
